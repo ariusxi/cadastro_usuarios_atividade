0dfce2789797976336f885d9423cc79f832c5d42
Alef Felix <alef.developerweb@gmail.com>
2020-10-06 21:17:10 -0300

    Implementing hawk on the user register

--- app/src/main/java/br/com/felix/cadastro_usuarios_atividade/LoginActivity.java
+++ app/src/main/java/br/com/felix/cadastro_usuarios_atividade/LoginActivity.java
@@ -5,13 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewNewRegister;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init(this).build();
+
+        this.textViewNewRegister = findViewById(R.id.textViewNewRegister);
+        this.buttonLogin = findViewById(R.id.buttonLogin);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Hawk.contains("has_register")) {
+            if (Hawk.get("has_register")) {
+                this.enableLogin();
+            } else {
+                this.disableLogin();
+            }
+        } else {
+            this.disableLogin();
+        }
+    }
+
+    private void enableLogin() {
+        this.textViewNewRegister.setVisibility(View.GONE);
+        this.buttonLogin.setEnabled(true);
+        this.buttonLogin.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    private void disableLogin() {
+        this.textViewNewRegister.setVisibility(View.VISIBLE);
+        this.buttonLogin.setEnabled(false);
+        this.buttonLogin.setBackgroundColor(getResources().getColor(R.color.gray));
     }
 
     public void newRegister(View view) {
